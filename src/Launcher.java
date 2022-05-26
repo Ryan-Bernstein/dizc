b50c4896065cd3be5c28ecc57b55010f9a32b579
BT_3W2_29 <ryanb67@nycstudents.net>
2022-05-26 10:21:09 -0400

    THIS I

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,18 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 public class Launcher {
-    public static void main(String[] args){
-        SwingUtilities.invokeLater(new Runnable() {
+    public static void main(String[] args) {
+        new Launcher();
+    }
+    /**
+     * Constructor for the Main object.
+     *
+     * @param f First name of this Person instance
+     * @param l Last name of this Person instance
+     */
+    public Launcher() {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 try {
-                    Game game = new Game("tim");
-                    game.createAndShowGui();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                    // ex.printStackTrace();
                 }
+
+                JFrame frame = new JFrame("ARM");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.add(new TestPane());
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+
             }
         });
+
     }
 }
