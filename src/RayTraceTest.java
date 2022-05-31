84d712cc025d654fbb17f8b60551d7a89e305a8a
BT_3W2_29 <ryanb67@nycstudents.net>
2022-05-31 10:53:23 -0400

    The thing

--- src/RayTraceTest.java
+++ src/RayTraceTest.java
@@ -1,26 +1,40 @@
 public class RayTraceTest {
-    private char[][] field = {{'#','0','#','#','#','0','0'},
-                              {'#','0','0','0','#','0','0'},
-                              {'#','#','#','0','#','0','0'},
-                              {'0','0','#','0','#','0','#'},
-                              {'#','0','0','0','0','0','#'},
-                              {'0','0','#','#','#','0','#'},
-                              {'#','0','0','0','0','0','#'},
-                              {'0','#','0','0','0','#','#'}};
+    private char[][] field = {{'#','.','#','#','#','.','.'},
+                              {'#','.','.','.','#','.','.'},
+                              {'#','#','#','.','#','.','.'},
+                              {'.','.','#','.','#','.','#'},
+                              {'#','.','.','.','.','.','#'},
+                              {'.','.','#','#','#','.','#'},
+                              {'#','.','.','.','.','.','#'},
+                              {'.','#','.','.','.','#','#'}};
     private Player player;
     private double FOV = Math.PI / 4;
     private int res;
+    private double rayRes;
 
     private double[] findRays(){
+        double[] out = new double[res];
         for(int i = 0; i < res; i++){
             double rayAngle = (player.getAng() - FOV / 2.0) + ((double) i / (double) res) * FOV;
-            double distanceToWall = 0; //abstract this
-            //while loop adding unit vector and checking
-            //add to an array
+            out[i] = findDist(rayAngle);
         }
-        return null;
+        return out;
+    }
+    private double findDist(double rayAngle){
+        double distanceToWall = 0;
+        double currX = player.getX(), currY = player.getY();
+        while(((int) currX > 0 && (int) currX < field.length)&& //in bounds X
+                ((int) currY > 0 && (int) currY < field[0].length)&&// in bounds Y
+                (field[(int) currX][(int) currY] == '.')) // no wall detected
+        {
+            currX += rayRes * Math.cos(rayAngle); //increment by the unit vector x
+            currY -= rayRes * Math.sin(rayAngle); //increment by the unit vector y -= because origin is top left
+            distanceToWall += rayRes;
+        }
+        return distanceToWall;
     }
 
-    private void genDraw(){//generate rects to draw from cols
+    private void genDraw(double[] dists){
+
     }
 }
